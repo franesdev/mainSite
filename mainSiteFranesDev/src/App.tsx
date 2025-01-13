246e150addb48fb1906a353347d1fba5bbc8768a
fpaute <fpaute@coaclamerced.fin.ec>
2025-01-12 21:44:16 -0500

    se cambia video de inicio

--- mainSiteFranesDev/src/App.tsx
+++ mainSiteFranesDev/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState, useRef } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import GenerateTest from './GenerateTest'; // Importa el nuevo componente
-import './App.css';
+import { useState, useRef } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import GenerateTest from "./GenerateTest"; // Importa el nuevo componente
+import "./App.css";
 import {
   FaFacebook,
   FaTwitter,
@@ -9,11 +9,11 @@ import {
   FaInstagram,
   FaTiktok,
   FaYoutube,
-} from 'react-icons/fa';
-import emailjs from 'emailjs-com';
-import image1 from '../public/logocircular.png';
-import image2 from '../public/logonombre.png';
-import YouTubePlayer from 'react-player/youtube';
+} from "react-icons/fa";
+import emailjs from "emailjs-com";
+import image1 from "../public/logocircular.png";
+import image2 from "../public/logonombre.png";
+import YouTubePlayer from "react-player/youtube";
 
 interface Translations {
   [key: string]: {
@@ -30,25 +30,25 @@ interface Translations {
 const translations: Translations = {
   en: {
     heroTitle: "I'm Franklin Paute Machuca",
-    heroSubtitle: 'Imagine | Code | Create',
+    heroSubtitle: "Imagine | Code | Create",
     heroText:
-      'Full Stack Developer | .NET | MAUI | React | 🇪🇨 Ecuadorian, passionate about what I do. I love tackling technical challenges and building innovative solutions. Sharing the journey of programming and software development.',
-    aboutTitle: 'Who is behind the keyboard?',
+      "Full Stack Developer | .NET | MAUI | React | 🇪🇨 Ecuadorian, passionate about what I do. I love tackling technical challenges and building innovative solutions. Sharing the journey of programming and software development.",
+    aboutTitle: "Who is behind the keyboard?",
     aboutText:
-      "With over 10 years of experience as a full-stack engineer, I\'m passionate about tackling complex challenges and delivering innovative solutions. I take a holistic approach, covering both front-end and back-end development, which allows me to build high-quality web and mobile applications. My technical skills include C#, Java, React, Xamarin Forms, .NET Core, and mobile development with .NET MAUI. I also have experience in database management and creating user-centered platforms. Always seeking new challenges, I\'m open to relocating for personal and professional growth.",
-    contactTitle: 'Are you looking for developing talent?',
-    contactText: 'I\'m here for new challenges.!',
+      "With over 10 years of experience as a full-stack engineer, I'm passionate about tackling complex challenges and delivering innovative solutions. I take a holistic approach, covering both front-end and back-end development, which allows me to build high-quality web and mobile applications. My technical skills include C#, Java, React, Xamarin Forms, .NET Core, and mobile development with .NET MAUI. I also have experience in database management and creating user-centered platforms. Always seeking new challenges, I'm open to relocating for personal and professional growth.",
+    contactTitle: "Are you looking for developing talent?",
+    contactText: "I'm here for new challenges.!",
   },
   es: {
-    heroTitle: 'Soy Franklin Paute Machuca',
-    heroSubtitle: 'Imagina | Programa | Crea',
+    heroTitle: "Soy Franklin Paute Machuca",
+    heroSubtitle: "Imagina | Programa | Crea",
     heroText:
-      'Desarrollador Full Stack | .NET | MAUI | React | 🇪🇨 Ecuatoriano, apasionado por lo que hago. Me encanta enfrentar desafíos técnicos y crear soluciones innovadoras. Compartinedo el viaje de la programación y el desarrollo de software.',
-    aboutTitle: '¿Quién está detrás del teclado?',
+      "Desarrollador Full Stack | .NET | MAUI | React | 🇪🇨 Ecuatoriano, apasionado por lo que hago. Me encanta enfrentar desafíos técnicos y crear soluciones innovadoras. Compartinedo el viaje de la programación y el desarrollo de software.",
+    aboutTitle: "¿Quién está detrás del teclado?",
     aboutText:
-      'Con más de 10 años de experiencia como ingeniero full-stack, me apasiona enfrentar retos complejos y ofrecer soluciones innovadoras. Tengo un enfoque integral que abarca tanto el desarrollo de front-end como back-end, permitiéndome crear aplicaciones web y móviles de alta calidad. Mis habilidades técnicas incluyen C#, Java, React, Xamarin Forms, .NET Core y el desarrollo móvil con .NET MAUI. Además, tengo experiencia en la gestión de bases de datos y en la creación de plataformas centradas en el usuario. Siempre en busca de nuevos desafíos, estoy abierto a reubicarme para seguir creciendo personal y profesionalmente.',
-    contactTitle: '¿Buscas talento en desarrollo?',
-    contactText: 'Estoy aquí para nuevos retos.',
+      "Con más de 10 años de experiencia como ingeniero full-stack, me apasiona enfrentar retos complejos y ofrecer soluciones innovadoras. Tengo un enfoque integral que abarca tanto el desarrollo de front-end como back-end, permitiéndome crear aplicaciones web y móviles de alta calidad. Mis habilidades técnicas incluyen C#, Java, React, Xamarin Forms, .NET Core y el desarrollo móvil con .NET MAUI. Además, tengo experiencia en la gestión de bases de datos y en la creación de plataformas centradas en el usuario. Siempre en busca de nuevos desafíos, estoy abierto a reubicarme para seguir creciendo personal y profesionalmente.",
+    contactTitle: "¿Buscas talento en desarrollo?",
+    contactText: "Estoy aquí para nuevos retos.",
   },
 };
 
@@ -64,7 +64,7 @@ function App() {
 }
 
 function Home() {
-  const [language, setLanguage] = useState<'en' | 'es'>('en');
+  const [language, setLanguage] = useState<"en" | "es">("en");
   const form = useRef(null);
 
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
@@ -72,10 +72,10 @@ function Home() {
     if (form.current) {
       emailjs
         .sendForm(
-          'service_2626zss',
-          'emailcontactportafolio',
+          "service_2626zss",
+          "emailcontactportafolio",
           form.current,
-          'u6wrgff2uwwCpj5hr'
+          "u6wrgff2uwwCpj5hr"
         )
         .then(
           (result) => {
@@ -88,7 +88,7 @@ function Home() {
 
       e.currentTarget.reset();
     } else {
-      console.error('Form reference is null.');
+      console.error("Form reference is null.");
     }
   };
 
@@ -102,7 +102,7 @@ function Home() {
     contactText,
   } = translations[language];
 
-  const changeLanguage = (lang: 'en' | 'es') => {
+  const changeLanguage = (lang: "en" | "es") => {
     const currentScrollY = window.scrollY; // Guarda la posición actual del scroll
     setLanguage(lang); // Cambia el idioma
     setTimeout(() => {
@@ -112,117 +112,117 @@ function Home() {
 
   return (
     <>
-      <div className='App'>
+      <div className="App">
         <header>
           <nav>
             <a
-              href='#'
-              onClick={() => changeLanguage('en')}
-              className={language === 'en' ? 'active' : ''}
+              href="#"
+              onClick={() => changeLanguage("en")}
+              className={language === "en" ? "active" : ""}
             >
               EN
             </a>
             <a
-              href='#'
-              onClick={() => changeLanguage('es')}
-              className={language === 'es' ? 'active' : ''}
+              href="#"
+              onClick={() => changeLanguage("es")}
+              className={language === "es" ? "active" : ""}
             >
               ES
             </a>
           </nav>
         </header>
-        <section className='hero'>
-          <div className='hero-content'>
+        <section className="hero">
+          <div className="hero-content">
             <h2>{heroSubtitle}</h2>
             <h1>{heroTitle}</h1>
             <p>{heroText}</p>
-            <div className='image-container'>
-              <img src={image1} alt='Image 1' className='image-1' />
-              <img src={image2} alt='Image 2' className='image-2' />
+            <div className="image-container">
+              <img src={image1} alt="Image 1" className="image-1" />
+              <img src={image2} alt="Image 2" className="image-2" />
             </div>
 
-            <div className='video-container'>
+            <div className="video-container">
               <YouTubePlayer
-                url='https://www.youtube.com/watch?v=n12-Btx-ABA'
+                url="https://youtu.be/CGAW7hNAyDE?si=YVjhEgJppdlyC6bd"
                 controls
                 loop
               />
             </div>
           </div>
         </section>
-        <section className='about'>
-          <div className='about-content'>
+        <section className="about">
+          <div className="about-content">
             <h2>{aboutTitle}</h2>
             <p>{aboutText}</p>
           </div>
         </section>
-        <section className='contact'>
+        <section className="contact">
           <h2>{contactTitle}</h2>
           <p>{contactText}</p>
-          <form ref={form} onSubmit={sendEmail} className='contact-form'>
+          <form ref={form} onSubmit={sendEmail} className="contact-form">
             <input
-              type='text'
-              name='user_name'
-              placeholder='Your Name'
+              type="text"
+              name="user_name"
+              placeholder="Your Name"
               required
             />
             <input
-              type='email'
-              name='user_email'
-              placeholder='Your Email'
+              type="email"
+              name="user_email"
+              placeholder="Your Email"
               required
             />
-            <textarea name='message' placeholder='Your Message' required />
-            <button type='submit'>Send</button>
+            <textarea name="message" placeholder="Your Message" required />
+            <button type="submit">Send</button>
           </form>
-          <div className='social-icons'>
+          <div className="social-icons">
             <a
-              href='https://facebook.com/franesdev'
-              target='_blank'
-              rel='noreferrer'
+              href="https://facebook.com/franesdev"
+              target="_blank"
+              rel="noreferrer"
             >
               <FaFacebook />
             </a>
             <a
-              href='https://twitter.com/franesdev'
-              target='_blank'
-              rel='noreferrer'
+              href="https://twitter.com/franesdev"
+              target="_blank"
+              rel="noreferrer"
             >
               <FaTwitter />
             </a>
             <a
-              href='https://linkedin.com/in/franesdev'
-              target='_blank'
-              rel='noreferrer'
+              href="https://linkedin.com/in/franesdev"
+              target="_blank"
+              rel="noreferrer"
             >
               <FaLinkedin />
             </a>
             <a
-              href='https://instagram.com/franesdev'
-              target='_blank'
-              rel='noreferrer'
+              href="https://instagram.com/franesdev"
+              target="_blank"
+              rel="noreferrer"
             >
               <FaInstagram />
             </a>
             <a
-              href='https://youtube.com/@franesdev'
-              target='_blank'
-              rel='noreferrer'
+              href="https://youtube.com/@franesdev"
+              target="_blank"
+              rel="noreferrer"
             >
               <FaYoutube />
             </a>
             <a
-              href='https://tiktok.com/@franesdev'
-              target='_blank'
-              rel='noreferrer'
+              href="https://tiktok.com/@franesdev"
+              target="_blank"
+              rel="noreferrer"
             >
               <FaTiktok />
             </a>
           </div>
         </section>
-        <footer className='footer'>
+        <footer className="footer">
           <p>© 2022-2024 FranesDev. All rights reserved.</p>
-          <a href='https://bio.link/franesdev' target='_blank' rel='noreferrer'>
+          <a href="https://bio.link/franesdev" target="_blank" rel="noreferrer">
             Mas por aquí
           </a>
         </footer>
